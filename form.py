13144726bdbe23c4200b987830f6b9bc2d7af903
DHARANRAJ <buvanbuvan31@gmail.com>
2026-09-21 09:58:18 +0530

    Add files via upload

---
+++ form.py
@@ -0,0 +1,20 @@
+name= "DHARANRAJ A"
+age= "17"
+dob = "31/01/2009"
+ads= "6/24,Bharathiyar Street, Alangulam"
+dis="Tenkasi"
+Blood="A+"
+id="2026BTAI038"
+pin="627851"
+height=5.7
+weight= "43 kg"
+print("NAME:",name)
+print("AGE:",age)
+print("Date of Birth:",dob)
+print("ID CARD NO:",id)
+print("Address:",ads)
+print("Pincode:",pin)
+print("District:",dis)
+print("Blood group:",Blood)
+print("Height:",height)
+print("Weight:",weight)
